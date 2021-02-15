f26197a90b983a948306a344cb6b89825296f1e6
Joel Magee <joelmagee@googlemail.com>
2021-02-15 23:14:48 +0000

    Update action script

--- .github/actions/pr-checklist-checker/index.js
+++ .github/actions/pr-checklist-checker/index.js
@@ -4,11 +4,13 @@ const github = require("@actions/github");
 const checklistRegex = /- \[ \] /g;
 
 async function run() {
+  core.info("PR checklist checker action run");
   try {
     const prBody = github.context.payload.pull_request.body;
+    core.debug("Pull request body content:", prBody);
 
-    if (!checklistRegex.test(prBody)) {
-      core.setFailed(error.message);
+    if (checklistRegex.test(prBody)) {
+      core.setFailed("There are some empty checklists");
     }
   } catch (error) {
     core.setFailed(error.message);
