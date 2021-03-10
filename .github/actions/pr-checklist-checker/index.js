bb86114fca8d2545965236b03a0d2f4e0595cf61
Joel Magee <joelmagee@googlemail.com>
2021-03-10 16:55:12 +0000

    Add node modules caching and ammend checklist check regex

--- .github/actions/pr-checklist-checker/index.js
+++ .github/actions/pr-checklist-checker/index.js
@@ -1,20 +1,21 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
-const checklistRegex = /- \[ \] /g;
+const checklistRegex = /(?:^|\n)\s*-\s+\[ \] /g;
 
 async function run() {
-  core.info("PR checklist checker action run");
+  core.info("PR checklist checker action has started");
   try {
     const prBody = github.context.payload.pull_request.body;
     core.debug("Pull request body content:", prBody);
 
     if (checklistRegex.test(prBody)) {
-      core.setFailed("There are some empty checklists");
+      core.setFailed("Not all checklist items have been completed");
     }
   } catch (error) {
     core.setFailed(error.message);
   }
+  core.info("PR checklist checker action has finished");
 }
 
 run();
--- pull_request_template.md
+++ pull_request_template.md
@@ -1,3 +1,4 @@
 ### PR Checklist
+
 - [ ] Check the size looks good on the front end
 - [ ] Check the image is centered
